4598c81ce2dc6eeae286c2b50d94d2cc61a69ee7
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 12:39:13 +0400

    Task3first character as the key

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task2 {
+
 }
+
+public int Task2(String W , String str , String word) {
+
+    Map<String, Integer> Word = new HashMap<>();
+
+    Word.put(W, 0);
+
+    for (String word ; str.length() ; i++) {
+
+        if (String word : str()) {
+
+        } else {
+
+            return Word;
+
+        }
+    }
+
+}
+
---
+++ src/task3.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task3 {
+
+}
+
+public int Task3(String f , String str , String l ) {
+
+    Map<String, String> pairs = new HashMap<>();
+
+    pairs.put(W, 0);
+
+    for (String srtf = f; str.length - 1; i++) {
+
+        if (String pairs:
+        str()){
+
+            return f;
+
+            for (String srtl = l; str.length - 1; i++) {
+
+                if (String pairs:
+                str()){
+
+                    return l;
+                }
+
+                return pairs;
+            }
+        }
+    }
+}
