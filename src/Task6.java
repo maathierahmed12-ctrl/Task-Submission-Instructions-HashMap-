f533d473c985ae0213c849904665516d81712910
maathier al-nabhani <maathier.ahmed12@gmail.com>
2026-04-20 14:28:33 +0400

    Task6Append a word to result string

---
+++ src/Task6.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task6 {
+
+}
+
+public int Task6( String str , String time , String[] arr) {
+
+    String result = "";
+
+    Map<String, String> wordAppend = new HashMap<>();
+
+    wordAppend.put(w, str);
+
+    for (String w : arr) {
+
+        String str = "" + wordAppend;
+
+        if (result.str(w) % 2 == 0) {
+
+        }
+
+         return result;
+
+    }
+}
